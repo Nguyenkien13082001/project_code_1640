66b7b2d913f4a553bdd6ad527626fb4aefe9ae88
Nguyenkien13082001 <kienntgbh190773@fpt.edu.vn>
2024-03-23 02:50:57 +0700

    prepare commit Heroku

---
+++ js/main2.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    let owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
